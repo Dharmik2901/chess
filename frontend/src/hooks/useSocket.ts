aa9a1a1bbb86d612f9615452564022f51839783e
Ritvik Raj <143361673+ritvikraj115@users.noreply.github.com>
2025-04-28 11:52:10 +0530

    Update useSocket.ts

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "ws://localhost:8080"
+const WS_URL = process.env.VITE_WS_URL
 
 export const useSocket = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
